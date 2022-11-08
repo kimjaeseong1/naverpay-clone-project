27b2390314e5d12736f7753c52e9fc1f6a412c9f
WONSEOKJE <nicholas4744@gmail.com>
2022-11-08 17:41:39 +0900

    feat: MemberService 구현

---
+++ src/main/java/org/example/members/service/IMemberService.java
@@ -0,0 +1,13 @@
+package org.example.members.service;
+
+import org.example.members.dto.MemberDTO;
+
+import java.util.List;
+
+public interface IMemberService {
+    MemberDTO login(String USERID);
+    MemberDTO login(String USERID, String USERPWD);
+    List<MemberDTO> findByUserId(String USERID);
+    MemberDTO getByUserId(String USERID);
+    List<MemberDTO> getAllUsers();
+}
---
+++ src/main/java/org/example/members/service/MemberService.java
@@ -0,0 +1,67 @@
+package org.example.members.service;
+
+import org.example.members.dao.MemberDAO;
+import org.example.members.dto.MemberDTO;
+import org.example.members.entity.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class MemberService implements IMemberService{
+
+    private MemberDAO memberDAO;
+
+    @Autowired
+    public MemberService(MemberDAO memberDAO) {
+        this.memberDAO = memberDAO;
+    }
+
+    @Override
+    public MemberDTO login(String USERID) {
+        MemberDTO memberDTO = new MemberDTO(USERID);
+        if (memberDTO == null) return null;
+
+        Member member = memberDAO.select(memberDTO.getUSERID());
+        return member.toDTO();
+    }
+
+    @Override
+    public MemberDTO login(String USERID, String USERPWD) {
+        MemberDTO memberDTO = new MemberDTO(USERID,USERPWD);
+        if (memberDTO == null) return null;
+
+        Member member = memberDAO.select(memberDTO.getUSERID());
+        if (member == null || member.getUSERPWD() == null) return null;
+        if (member.getUSERPWD().equals(memberDTO.getUSERPWD())) {
+            return memberDTO;
+        }
+        return null;
+    }
+
+    @Override
+    public List<MemberDTO> findByUserId(String USERID) {
+        if (USERID == null) return null;
+
+        List<Member> memberList = memberDAO.search(USERID);
+        return memberList.stream().map(m -> m.toDTO()).collect(Collectors.toList());
+    }
+
+    @Override
+    public MemberDTO getByUserId(String USERID) {
+        if (USERID == null) return null;
+
+        Member member = memberDAO.select(USERID);
+        if (member == null) return null;
+
+        return member.toDTO();
+    }
+
+    @Override
+    public List<MemberDTO> getAllUsers() {
+        List<Member> memberList = memberDAO.selectAll();
+        return memberList.stream().map(m -> m.toDTO()).collect(Collectors.toList());
+    }
+}
