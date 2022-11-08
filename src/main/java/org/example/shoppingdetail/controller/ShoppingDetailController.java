f6c30094db98b331bcd73ba161870e9dabb879b1
yujaemin <woawoa0513@gmail.com>
2022-11-08 21:54:35 +0900

    feat: feat: Controller 개발
    미완성

--- src/main/java/org/example/shoppingdetail/controller/ShoppingDetailController.java
+++ src/main/java/org/example/shoppingdetail/controller/ShoppingDetailController.java
@@ -1,16 +1,34 @@
 package org.example.shoppingdetail.controller;
 
+import org.example.shoppingdetail.dto.ShoppingDetailDTO;
+import org.example.shoppingdetail.service.ShoppingDetailService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/naver")
 public class ShoppingDetailController {
+    private ShoppingDetailService shoppingDetailService = ShoppingDetailService.getInstance();
 
     /**
      * 쇼핑 상세 페이지 접근
      */
+    @GetMapping("/detail")
+    public String shoppingDetailPage(Model model, @RequestParam int paymentId) {
+//        if (paymentId < 1) {
+//            return
+//        }
+        ShoppingDetailDTO shoppingDetailDTO = shoppingDetailService.getShoppingDetailByPaymentId(paymentId);
 
+        model.addAttribute(shoppingDetailDTO);
+
+        return "shoppingdetail";
+    }
 
     /**
      * 쇼핑 상세 리스트 반환
