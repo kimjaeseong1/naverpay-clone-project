b45f2f417b5bef6c6476e21bbfcebbe989e55b47
yujaemin <woawoa0513@gmail.com>
2022-11-09 19:51:58 +0900

    feat: 쇼핑 상세 정보 컨트롤러 구현

--- src/main/java/org/example/database/JDBCMgr.java
+++ src/main/java/org/example/database/JDBCMgr.java
@@ -12,7 +12,7 @@ public class JDBCMgr {
         try {
             DriverManager.registerDriver(new org.h2.Driver());
 
-            conn = DriverManager.getConnection("jdbc:h2:C:/Users/AdmiN/Documents/dev/FAST_JAVA/naverpay-clone-project/NaverPayDB", "sa", "");
+            conn = DriverManager.getConnection("jdbc:h2:C:/workspace/naverpay-local/naverpay-clone-project/NaverPayDB;OLD_INFORMATION_SCHEMA=TRUE", "sa", "");
 
         } catch (SQLException e) {
             e.printStackTrace();
--- src/main/java/org/example/shoppingdetail/controller/ShoppingDetailController.java
+++ src/main/java/org/example/shoppingdetail/controller/ShoppingDetailController.java
@@ -1,17 +1,15 @@
 package org.example.shoppingdetail.controller;
 
+import org.example.shoppingdetail.dao.ShoppingDetailDAO;
 import org.example.shoppingdetail.dto.ShoppingDetailDTO;
+import org.example.shoppingdetail.entity.ShoppingDetail;
 import org.example.shoppingdetail.service.ShoppingDetailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/naver")
+@RequestMapping("/naver/pay/shopping")
 public class ShoppingDetailController {
     private ShoppingDetailService shoppingDetailService = ShoppingDetailService.getInstance();
 
@@ -20,22 +18,26 @@ public class ShoppingDetailController {
      */
     @GetMapping("/detail")
     public String shoppingDetailPage(Model model, @RequestParam int paymentId) {
-//        if (paymentId < 1) {
-//            return
-//        }
-        ShoppingDetailDTO shoppingDetailDTO = shoppingDetailService.getShoppingDetailByPaymentId(paymentId);
+        ShoppingDetail shoppingDetail = shoppingDetailService.getShoppingDetailByPaymentId(paymentId).toEntity();
+        model.addAttribute("shoppingDetail", shoppingDetail);
 
-        model.addAttribute(shoppingDetailDTO);
-
-        return "shoppingdetail";
+//        String paymentMethod = shoppingDetail.getPaymentMethod();
+//        model.addAttribute("paymentMethod", shoppingDetail);
+//
+//
+//        model.addAttribute("orderStatus", shoppingDetail);
+        return "shoppingdetail/shoppingdetail";
     }
-
-    /**
-     * 쇼핑 상세 리스트 반환
-     */
-
 
     /**
      * 쇼핑 상세 리스트 삭제
      */
+    @PostMapping("/delete")
+    public String shoppingDetailDelete(@RequestBody String paymentId) {
+        String valueStr = paymentId.substring(10, paymentId.length());
+        int value = Integer.parseInt(valueStr);
+        boolean isRemoved = shoppingDetailService.deleteShoppingDetailByPaymentId(value);
+        return "redirect:/naver/pay/order"; // 주문 리스트로 바꿀 예정
+    }
+
 }
