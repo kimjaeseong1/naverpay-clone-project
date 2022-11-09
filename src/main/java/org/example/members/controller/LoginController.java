3e3967d993d2ba7a2547aad952c0a83f0e584976
WONSEOKJE <nicholas4744@gmail.com>
2022-11-09 19:36:06 +0900

    fix: 프론트 부분 수정 홈페이지 및 로그인페이지 변경

--- src/main/java/org/example/members/controller/LoginController.java
+++ src/main/java/org/example/members/controller/LoginController.java
@@ -72,7 +72,6 @@ public class LoginController {
             respStatus = Status.SUCCESS;
         }
 
-
         session.setAttribute("login",respStatus);
         model.addAttribute("USERID", session.getAttribute("SESSION_ID"));
         return view;
